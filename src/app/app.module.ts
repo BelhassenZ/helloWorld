273c1db8c9c51f48d463e609aae161d841d40312
BelhassenZ <zinelabidineb@gmail.com>
2018-03-08 16:42:11 +0100

    Added angular firebase with testing

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,10 @@
 import { NgModule, ErrorHandler } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { IonicApp, IonicModule, IonicErrorHandler } from 'ionic-angular';
+
+import { AngularFireModule } from 'angularfire2';
+import { AngularFireDatabaseModule } from 'angularfire2/database';
+
 import { MyApp } from './app.component';
 
 import { AboutPage } from '../pages/about/about';
@@ -10,6 +14,15 @@ import { TabsPage } from '../pages/tabs/tabs';
 
 import { StatusBar } from '@ionic-native/status-bar';
 import { SplashScreen } from '@ionic-native/splash-screen';
+
+const angularFireConfig = {
+  apiKey: "AIzaSyDIiPO0we1KFf7zZKGxppKHyXRHcqh2rXM",
+  authDomain: "hello-world-3c256.firebaseapp.com",
+  databaseURL: "https://hello-world-3c256.firebaseio.com",
+  projectId: "hello-world-3c256",
+  storageBucket: "hello-world-3c256.appspot.com",
+  messagingSenderId: "961974208287"
+};
 
 @NgModule({
   declarations: [
@@ -21,7 +34,9 @@ import { SplashScreen } from '@ionic-native/splash-screen';
   ],
   imports: [
     BrowserModule,
-    IonicModule.forRoot(MyApp)
+    IonicModule.forRoot(MyApp),
+    AngularFireModule.initializeApp(angularFireConfig),
+    AngularFireDatabaseModule
   ],
   bootstrap: [IonicApp],
   entryComponents: [
--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,5 +1,7 @@
 import { Component } from '@angular/core';
 import { NavController } from 'ionic-angular';
+import { AngularFireDatabase, AngularFireList } from 'angularfire2/database';
+import { Observable } from 'rxjs/Observable';
 
 @Component({
   selector: 'page-home',
@@ -7,8 +9,28 @@ import { NavController } from 'ionic-angular';
 })
 export class HomePage {
 
-  constructor(public navCtrl: NavController) {
 
+  itemsRef: AngularFireList<any>;
+  items: Observable<any[]>;
+  constructor(public navCtrl: NavController, db: AngularFireDatabase) {
+    this.itemsRef = db.list('messages');
+    // Use snapshotChanges().map() to store the key
+    this.items = this.itemsRef.snapshotChanges().map(changes => {
+      return changes.map(c => ({ key: c.payload.key, ...c.payload.val() }));
+    });
   }
+  addItem(newName: string) {
+    this.itemsRef.push({ text: newName });
+  }
+  updateItem(key: string, newText: string) {
+    this.itemsRef.update(key, { text: newText });
+  }
+  deleteItem(key: string) {    
+    this.itemsRef.remove(key); 
+  }
+  deleteEverything() {
+    this.itemsRef.remove();
+  }
+
 
 }
